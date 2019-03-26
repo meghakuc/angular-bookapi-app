24878422766f5fb273a2a6d938f0f42d7778a544
meghakuc <meghakuchhal@gmail.com>
2019-03-26 21:18:59 +0530

    Update book-detail.component.spec.ts

--- src/app/book-detail/book-detail.component.spec.ts
+++ src/app/book-detail/book-detail.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { BookDetailComponent } from './book-detail.component';
+import { FormsModule } from '@angular/forms';
+import { RestApiService } from "../rest-api.service";
+import { HttpClientModule } from '@angular/common/http';
 
 describe('BookDetailComponent', () => {
   let component: BookDetailComponent;
@@ -8,18 +11,27 @@ describe('BookDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookDetailComponent ]
+	  imports: [ RouterTestingModule, FormsModule, HttpClientModule ],
+      declarations: [ BookDetailComponent ],
+	  providers: [RestApiService]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookDetailComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should display a message says there is no book detail.', async(() => {
+    const fixture = TestBed.createComponent(BookDetailComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain('There is no book detail yet!');
+  }));
 });
