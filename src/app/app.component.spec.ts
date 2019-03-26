ee4905849224c4aaa9ec01f5fa99cd826a84281a
meghakuc <meghakuchhal@gmail.com>
2019-03-26 21:15:47 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-bookapi-app'`, () => {
+  it(`should have as title 'angular-bookapi-app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-bookapi-app');
-  });
+  }));
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a h5 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-bookapi-app!');
-  });
+    expect(compiled.querySelector('h5').textContent).toContain('Books Management');
+  }));
 });
