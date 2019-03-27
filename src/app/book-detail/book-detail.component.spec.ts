1dffb1cff06c796a88bed1b39157e9068fa02111
meghakuc <meghakuchhal@gmail.com>
2019-03-27 14:42:46 +0530

    Update book-detail.component.spec.ts

--- src/app/book-detail/book-detail.component.spec.ts
+++ src/app/book-detail/book-detail.component.spec.ts
@@ -28,10 +28,33 @@ describe('BookDetailComponent', () => {
     expect(component).toBeTruthy();
   });
   
-  it('should display a message says there is no book detail.', async(() => {
+  it('should display a message says "there is no book detail yet!".', async(() => {
     const fixture = TestBed.createComponent(BookDetailComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('p').textContent).toContain('There is no book detail yet!');
+  }));
+  
+  it('should allow us to set ISBN value in input field', async(() => {
+    setInputValue('#text1', '9789000010134');
+	fixture.whenStable().then(() => {
+		expect(component.ISBN).toEqual('9789000010134');
+	});
+  }));
+
+  function setInputValue(selector: string, value: string) {
+    fixture.detectChanges();
+    let input = fixture.debugElement.nativeElement.querySelector('input');
+    input.value = value;
+    input.dispatchEvent(new Event('input'));
+  }
+  
+  it('should call "getBookData" method', async(() => {
+	spyOn(component, 'getBookData');
+	let button = fixture.debugElement.nativeElement.querySelector('button');
+	button.click();
+	fixture.whenStable().then(() => {
+		expect(component.getBookData).toHaveBeenCalled();
+	});
   }));
 });
